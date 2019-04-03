da35cb73e181f0fe19f1bdad6d3e2bdd8a7f1f08
DFFGmicros <49163666+DFFGmicros@users.noreply.github.com>
2019-04-03 16:29:17 -0600

    Add files via upload
    Codigo para testing

---
+++ Tarea2Testing.py
@@ -0,0 +1,14 @@
+import Tarea2Metodos
+
+def test_Palindromo():
+    assert Tarea2Metodos.Palindromo() == 1
+
+
+def test_Array():
+    assert Tarea2Metodos.Array() == 1
+
+
+def test_LecturaTxt():
+    assert Tarea2Metodos.LecturaTxt() == 1
+
+    
